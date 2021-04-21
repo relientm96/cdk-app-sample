94e76132eb672cfab9ee83031ff318f7b4a740cd
Matthew Yong <myong@seek.com.au>
2021-04-21 12:40:08 +1000

    Adding simple functionality to stand up a lambda

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,18 +1,19 @@
 import * as sns from '@aws-cdk/aws-sns';
 import * as subs from '@aws-cdk/aws-sns-subscriptions';
 import * as sqs from '@aws-cdk/aws-sqs';
+import * as lambda from '@aws-cdk/aws-lambda';
 import * as cdk from '@aws-cdk/core';
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
-    });
-
-    const topic = new sns.Topic(this, 'CdkWorkshopTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    // Pull in lambda handler from lambda
+    const helloLambda = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_10_X,   // execution environment
+      code: lambda.Code.fromAsset('lambda'), // code loaded from '/lambda/hello.js'
+      handler: 'hello.handler',
+    })
+    
   }
 }
